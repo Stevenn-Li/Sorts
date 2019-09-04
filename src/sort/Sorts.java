1369e57e197c321f3728a22ab5106bf24533ceb4
Steven Li <sxli@buffalo.edu>
2019-09-03 23:53:02 -0400

    Added Quicksort and extra comments

--- src/sort/Sorts.java
+++ src/sort/Sorts.java
@@ -64,6 +64,16 @@ public class Sorts {
 		}		
 	}
 	
+	/* 
+	 * Insertion Sort:
+	 * Loop traverses through an array, comparing two elements at a time
+	 * It compares two elements, if the first one is smaller than the second
+	 * Then we know it is in the right order, and we consider that subarray to be sorted
+	 * Next we compare, the second and third element and so on.
+	 * If an element is smaller than the previous, we will put it in compare it to
+	 * elements in the sorted array to place it where it belongs
+	 * Runtime: Best: O(n) || Avg: O(n^2) || Worst: O(n^2) 
+	 */
 	public void insertionSort(int[] arr) {
 		for (int i = 1; i < arr.length; i++) {
 			int idx = arr[i];
@@ -80,18 +90,55 @@ public class Sorts {
 		
 	}
 	
-	public void quickSort(int[] arr) {
-
+	/* 
+	 * Partition for quickSort:
+	 * Uses the last element in the array as a pivot, puts the pivot into its sorted position
+	 * by using low and high to find its correct spot via numbers smaller than it and larger than it.
+	 * Elements smaller are on the pivot's left, and greater on its right
+	 */
+	public int partition(int[] arr, int low, int high) {
+		
+		int pivot = arr[high];
+		int lowIdx = low - 1;
+		
+		for (int i = low; i < high; i++) {
+			if (arr[i] < pivot) {
+				lowIdx++;
+				int temp = arr[lowIdx];
+				arr[lowIdx] = arr[i];
+				arr[i] = temp;
+			}
+		}
+		
+		int temp = arr[lowIdx+1];
+		arr[lowIdx+1] = arr[high];
+		arr[high] = temp;
+		
+		return lowIdx+1;
+	}
+	/* 
+	 * QuickSort:
+	 * Recursively sorts array by partitioning it using a pivot in order to get elements
+	 * smaller than it and larger than it
+	 * Runtime: Best: O(n log(n)) || Avg: O(n log(n)) || Worst: O(n^2) 
+	 */
+	public void quickSort(int[] arr, int low, int high) {
+		
+		if (low < high) {
+			int pivot = partition(arr, low, high);
+			quickSort(arr, low, pivot - 1);
+			quickSort(arr, pivot+1, high);
+		}
 	}
 	
 	public void heapSort(int[] arr) {
-
+		
 	}
 	
 	public static void main(String[] args) {
 		Sorts s = new Sorts();
 		int[] unsortedArray = s.randomArray(100, 100);
-		s.bubbleSort(unsortedArray);
+		s.quickSort(unsortedArray, 0, unsortedArray.length-1);
 		for (int i = 0; i < unsortedArray.length; i++) {
 			System.out.println(unsortedArray[i]);
 		}
